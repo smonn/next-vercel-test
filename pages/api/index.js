653074d9b0bab0ca1f21ffca38fc3de3a19b9c6a
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-16 15:51:01 -0400

    another attempt at using root dir

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,14 +2,14 @@ import path from "path";
 import fs from "fs";
 
 export default function handle(req, res) {
-  const nextDir = path.join(process.cwd(), ".next");
+  const directory = path.join(process.cwd());
 
-  console.log(".next dir", nextDir);
-  console.log("files", fs.readdirSync(nextDir));
+  console.log(".next dir", directory);
+  console.log("files", fs.readdirSync(directory));
   console.log("");
 
   const data = JSON.parse(
-    fs.readFileSync(path.join(nextDir, process.env.MESSAGE_FILE_NAME), "utf8")
+    fs.readFileSync(path.join(directory, process.env.MESSAGE_FILE_NAME), "utf8")
   );
 
   res.json({
