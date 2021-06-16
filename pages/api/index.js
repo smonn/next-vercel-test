208ae1f5fa57f124f6dc320c8d091d090dc638f1
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-16 15:37:20 -0400

    also cwd

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,6 +4,10 @@ import fs from "fs";
 export default function handle(req, res) {
   console.log("__dirname", __dirname);
   console.log("files", fs.readdirSync(__dirname));
+  console.log("");
+  console.log("cwd", process.cwd());
+  console.log("files", fs.readdirSync(process.cwd()));
+  console.log("");
 
   const data = JSON.parse(
     fs.readFileSync(path.join(__dirname, process.env.MESSAGE_FILE_NAME), "utf8")
