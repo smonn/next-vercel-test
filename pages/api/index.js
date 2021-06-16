8d2c8c4a1b257c535cb513d58f2d07e64d52f6cd
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-16 15:22:38 -0400

    dynamic file name

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 export default function handle(req, res) {
   const data = JSON.parse(
-    fs.readFileSync(path.resolve("./message.json"), "utf8")
+    fs.readFileSync(path.resolve(`./${process.env.MESSAGE_FILE_NAME}`), "utf8")
   );
 
   res.json({
