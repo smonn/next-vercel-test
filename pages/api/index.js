e9f6d7bb249c3eb9c396778a8a5bd5788efb5bb7
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-16 15:28:24 -0400

    Write file to `.next` directory

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,10 @@ import fs from "fs";
 
 export default function handle(req, res) {
   const data = JSON.parse(
-    fs.readFileSync(path.resolve(`./${process.env.MESSAGE_FILE_NAME}`), "utf8")
+    fs.readFileSync(
+      path.resolve(`./.next/${process.env.MESSAGE_FILE_NAME}`),
+      "utf8"
+    )
   );
 
   res.json({
